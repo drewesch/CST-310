9467ac2e2ffc81e90120290921520fd5b8fcb02a
Drew <wheels.esch@gmail.com>
2022-09-11 16:59:32 -0700

    Final project upload

--- Sierpinski-Gasket/Project/2D-Sierpinski-OpenGL.cpp
+++ Sierpinski-Gasket/Project/2D-Sierpinski-OpenGL.cpp
@@ -1,67 +1,65 @@
-// This program generates a Sierpinski gasket with 10000 points.
+/*
+Project 1: Sierpinski Gasket - Example 1 (2D Gasket)
+
+Authors: Andrew Esch and Evan Lee
+
+Date: 9/11/2022
+*/
 
 #ifdef __APPLE_CC__
-#include <GLUT/glut.h>
+#include <GLUT/glut.h> // If using MAC/UNIX architecture, import GLUT/glut.h
 #else
-#include <GL/glut.h>
+#include <GL/glut.h> // if using other architecutres, import GL/glut.h
 #endif
-#include <cstdlib>
+#include <cstdlib> // Import cstdlib
 
-// A simple two-dimensional point class to make life easy.  It allows you to
-// reference points with x and y coordinates instead of array indices) and
-// encapsulates a midpoint function.
+
+// Define a 2D point struct to reference x and y coordinates
 struct Point {
-  GLfloat x, y;
-  Point(GLfloat x = 0, GLfloat y = 0): x(x), y(y) {}
-  Point midpoint(Point p) {return Point((x + p.x) / 2.0, (y + p.y) / 2.0);}
+  GLfloat x, y; // Define GLfloat coordinate variables
+  Point(GLfloat x = 0, GLfloat y = 0): x(x), y(y) {} // Set coordinates to 0 by default
+  Point midpoint(Point p) {return Point((x + p.x) / 2.0, (y + p.y) / 2.0);} // Encapsulate a midpoint function for x and y
 };
 
-// Draws a Sierpinski triangle with a fixed number of points. (Note that the
-// number of points is kept fairly small because a display callback should
-// NEVER run for too long.
+// Main program display function
+// Draws the Sierpinski Gasket with a finite number of points
 void display() {
 
-  glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT); // Clear color buffer bit
 
-  // static Point vertices[] = {Point(0, 0), Point(250, 500), Point(500, 0)};
-  static Point vertices[] = {Point(100, 100), Point(250, 500), Point(400, 100)};
+  static Point vertices[] = {Point(100, 100), Point(250, 500), Point(400, 100)}; // Create a random set of points within the window, this is the verticies for the main triangle
 
-  // Compute and plot 100000 new points, starting (arbitrarily) with one of
-  // the vertices. Each point is halfway between the previous point and a
-  // randomly chosen vertex.
-  static Point p = vertices[0];
-  glBegin(GL_POINTS);
+  // Compute and plot 100000 new points of the Sierpinski Gasket
+  static Point p = vertices[0]; // Set the initial value as an arbitary vertice (use index 0, 1, or 2)
+  glBegin(GL_POINTS); // Begin plotting in OpenGL
   for (int k = 0; k < 100000; k++) {
-    p = p.midpoint(vertices[rand() % 3]);
-    glVertex2f(p.x, p.y);
+    p = p.midpoint(vertices[rand() % 3]); // Choose a random vertice and find the midpoint between current initial value and the chosen vertice
+    glVertex2f(p.x, p.y); // Plot the vertice in OpenGL
   }
-  glEnd();
-  glFlush();
+  glEnd(); // End plotting
+  glFlush(); // Flush OpenGL points
 }
 
-// Performs application-specific initialization. Sets colors and sets up a
-// simple orthographic projection.
+// Initial program setup function for application-specific integrations
 void init() {
 
-  // Set a deep purple background and draw in a greenish yellow.
-  glClearColor(0.25, 0.0, 0.2, 1.0);
-  glColor3f(0.6, 1.0, 0.0);
+  glClearColor(0.25, 0.0, 0.2, 1.0); // Set a deep purple background in OpenGL
+  glColor3f(0.6, 1.0, 0.0); // Draw the Sierpinski Gasket using yellow color.
 
   // Set up the viewing volume: 500 x 500 x 1 window with origin lower left.
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(0.0, 500.0, 0.0, 500.0, 0.0, 1.0);
+  glMatrixMode(GL_PROJECTION); // Set the projection mode as orthographic
+  glLoadIdentity(); // Load identity matrix
+  glOrtho(0.0, 500.0, 0.0, 500.0, 0.0, 1.0); // Set orthographic plane
 }
 
-// Initializes GLUT, the display mode, and main window; registers callbacks;
-// does application initialization; enters the main event loop.
+// Initialize GLUT and OpenGL for main loop
 int main(int argc, char** argv) {
-  glutInit(&argc, argv);
-  glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize(750, 750);
-  glutInitWindowPosition(0, 0);
-  glutCreateWindow("2D Sierpinski Gasket Example - by Andrew Esch & Evan Lee");
-  glutDisplayFunc(display);
-  init();
-  glutMainLoop();
+  glutInit(&argc, argv); // Intialize OpenGL using main function pointers
+  glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB); // Set OpenGL display mode to single and RBG
+  glutInitWindowSize(750, 750); // Set window size to 750 by 750, can be modified by user
+  glutInitWindowPosition(0, 0); // Set window position to 0, 0; can be modified by user
+  glutCreateWindow("2D Sierpinski Gasket Example - by Andrew Esch & Evan Lee"); // Set window title
+  glutDisplayFunc(display); // Load display function as register callback
+  init(); // Run initial function
+  glutMainLoop(); // Run OpenGL as register callback until the user closes the window 
 }
--- Sierpinski-Gasket/Project/3D-Sierpinski-OpenGL.cpp
+++ Sierpinski-Gasket/Project/3D-Sierpinski-OpenGL.cpp
@@ -1,95 +1,86 @@
-// This program is a modification and extension of an early program in Edward
-// Angel's book, Interactive Computer Graphics: A top-down approach using
-// OpenGL.  It plots the 3-D Sierpinski Tetrahedron using colors in a nice
-// way.
+/*
+Project 1: Sierpinski Gasket - Example 2 (3D Gasket)
+
+Authors: Andrew Esch and Evan Lee
+
+Date: 9/11/2022
+*/
 
 #ifdef __APPLE_CC__
-#include <GLUT/glut.h>
+#include <GLUT/glut.h> // If using MAC/UNIX architecture, import GLUT/glut.h
 #else
-#include <GL/glut.h>
+#include <GL/glut.h> // if using other architecutres, import GL/glut.h
 #endif
-#include <cstdlib>
+#include <cstdlib> // Import cstdlib
 
-// A simple three-dimensional point class to make life easy.  It allows you
-// to reference points with x and y coordinates instead of array indices) and
-// encapsulates a midpoint function.
+// Define a 3D point struct to reference x, y, and z coordinates
 struct Point {
-  GLfloat x, y, z;
-  Point(GLfloat x, GLfloat y, GLfloat z): x(x), y(y), z(z) {}
-  Point midpoint(Point p) {return Point((x+p.x)/2, (y+p.y)/2, (z+p.z)/2);}
+  GLfloat x, y, z; // Define GLfloat coordinate variables
+  Point(GLfloat x, GLfloat y, GLfloat z): x(x), y(y), z(z) {} // Set coordinates to 0 by default
+  Point midpoint(Point p) {return Point((x+p.x)/2, (y+p.y)/2, (z+p.z)/2);} // Encapsulate a midpoint function for x, y, and z
 };
 
-// Handles reshape requests by setting the the viewport to exactly match the
-// pixel coordinates, so we can draw in the whole window.  Then sets up a
-// simple perspective viewing volume to ensure that the pyramid will never
-// be distorted when the window is reshaped.  The particular settings chosen
-// ensure that the vertical extent of the pyramid will always be visible.
+// Reshape function for dynamic 3D display
 void reshape(GLint w, GLint h) {
-  glViewport(0, 0, w, h);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  gluPerspective(100.0, GLfloat(w)/GLfloat(h), 10.0, 1500.0);
+  glViewport(0, 0, w, h); // Set viewport based on defined width and height
+  glMatrixMode(GL_PROJECTION); // Set orthographic projection mode
+  glLoadIdentity(); // Load identity matrix
+  gluPerspective(100.0, GLfloat(w)/GLfloat(h), 10.0, 1500.0); // Set dynamic perspective viewing volumne
 }
 
-// Handles display requests.  All it has to do is clear the viewport because
-// the real drawing is done in the idle callback.
+// Main program display function
+// Clears OpenGL color buffers
 void display() {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear color buffer bit and depth (for 3D Gasket)
 }
 
 // Draws the "next 500 points".  The function contains locally the definitions
 // of the vertices and the current point as static objects so that their values
 // are retained between calls.
-void generateMorePoints() {
+void generatePoints() {
 
-  // The tetrahedron has four vertices.  We also have to keep track of the
-  // current point during the plotting.
-  static Point vertices[4] = {
+
+  static Point vertices[4] = { // Create a random set of points within the window, this is the verticies for the main tetrahedron
     Point(-350, -225, -200),
     Point(-150, -400, -600),
     Point(250, -225, -300),
     Point(0, 450, -400)
   };
-  static Point lastPoint = vertices[0];
+  static Point lastPoint = vertices[0]; // Set the initial value as an arbitary vertice (use index 0, 1, 2, or 3)
 
-  // Here is the code to draw the "next 500 points".  The closer the point is
-  // to the camera, the brighter we make it.  The coloring technique is a
-  // quick hack which (unprofessionally) depends on the fact that the range
-  // of z-coordinates in the tetrahedron run from -200 to -700.  By adding
-  // 700 to any z-value and then dividing by 500, we get values in the range
-  // 0 to 1 - just perfect for coloring.
+  // Compute and plot 1000 new points of the Sierpinski Gasket
   glBegin(GL_POINTS);
   for (int i = 0; i <= 1000; i++) {
-    lastPoint = lastPoint.midpoint(vertices[rand() % 4]);
-    GLfloat intensity = (600 + lastPoint.z) / 400.0;
-    glColor3f(intensity, intensity, 0.25);
-    glVertex3f(lastPoint.x, lastPoint.y, lastPoint.z);
+    lastPoint = lastPoint.midpoint(vertices[rand() % 4]); // Choose a random vertice and find the midpoint between current initial value and the chosen vertice
+    GLfloat intensity = (600 + lastPoint.z) / 400.0; // Set color intensity variable based on distance, this gives the "3D" effect
+    glColor3f(intensity, intensity, 0.25); // Set color intensity 
+    glVertex3f(lastPoint.x, lastPoint.y, lastPoint.z); // Plot the vertice in OpenGL
   }
-  glEnd();
-  glFlush();
+  glEnd(); // End plotting
+  glFlush(); // Flush OpenGL points
 }
 
-// Performs application-specific initialization.  In this program we want to
-// do depth buffering, which has to be explicitly enabled in OpenGL.
+// Initial program setup function for application-specific integrations
+// Implements depth buffering
 void init() {
-  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_DEPTH_TEST); // Depth buffering
 
   // Set a blue background
   glClearColor(0.0, 0.0, 1.0, 1.0);
   glColor3f(1.0, 1.0, 1.0);
 }
 
-// Initializes GLUT, the display mode, and main window; registers callbacks;
-// does application initialization; enters the main event loop.
+// Initialize GLUT and OpenGL for main loop
 int main(int argc, char** argv) {
-  glutInit(&argc, argv);
-  glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-  glutInitWindowSize(750, 750);
-  glutInitWindowPosition(0, 0);
+  glutInit(&argc, argv); // Intialize OpenGL using main function pointers
+  glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH); // Set OpenGL display mode to single, RBG, and depth mode
+  glutInitWindowSize(750, 750); // Set window size to 750 by 750, can be modified by user
+  glutInitWindowPosition(0, 0); // Set window position to 0, 0; can be modified by user
   glutCreateWindow("Sierpinski Gasket Tetrahedron Example - by Andrew Esch & Evan Lee");
-  glutDisplayFunc(display);
-  glutReshapeFunc(reshape);
-  glutIdleFunc(generateMorePoints);
-  init();
-  glutMainLoop();
+  glutDisplayFunc(display); // Load display function as register callback
+  glutMainLoop(); // Run OpenGL as register callback until the user closes the window 
+  glutReshapeFunc(reshape); // Initialize reshape function
+  glutIdleFunc(generatePoints);
+  init(); // Run initial function
+  glutMainLoop(); // Run OpenGL as register callback until the user closes the window 
 }
