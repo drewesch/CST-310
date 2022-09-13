5e534cb6f24e61c4eb68ef33536c51178b20ba16
EvanLee25 <evan.lee25@outlook.com>
2022-09-13 11:25:10 -0700

    added before and after for fixbugs.cpp

--- Your-Surrounding-World/class-examples/fixbugs.cpp
+++ Your-Surrounding-World/class-examples/fixbugs.cpp
@@ -1,3 +1,28 @@
+/* BEFORE WITH BUGS
+#include <glut.h>  <--- Need to use <GL/glut.h>
+void renderScene(void) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+        glVertex3f(-0.5,-0.5,0.0);
+        glVertex3f(0.5,0.0,0.0);
+        glVertex3f(0.0,0.5,0.0);
+    glEnd();
+    glFlush();
+}
+
+void main(int argc, char **argv) { <--- Need to use int main
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_SINGLE | GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(320,320);
+    glutCreateWindow(“GCU – My Triangle");
+    glutDisplayFunc(renderScene);
+    glutMainLoop();
+                <--- Need to return 0;
+}
+*/
+
+// AFTER BUG FIX
 #include <GL/glut.h>
 void renderScene(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
