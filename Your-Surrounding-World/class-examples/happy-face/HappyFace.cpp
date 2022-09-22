6c00a4c2cdae7df27c43d8757a6adead97dc6b2e
EvanLee25 <evan.lee25@outlook.com>
2022-09-22 11:50:52 -0700

    white bkgd

--- Your-Surrounding-World/class-examples/happy-face/HappyFace.cpp
+++ Your-Surrounding-World/class-examples/happy-face/HappyFace.cpp
@@ -106,12 +106,14 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // Load, create texture and generate mipmaps
     int width, height;
-    
-    //unsigned char* image = SOIL_load_image("Container1.jpg", &width, &height, 0, SOIL_LOAD_RGB);
-    //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    //glGenerateMipmap(GL_TEXTURE_2D);
-    //SOIL_free_image_data(image);
-    //glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
+    int scope = 0;
+    width = 1000;
+    height = 1000;
+    unsigned char* image = SOIL_load_image("white.jpg", &width,&height, &scope, SOIL_LOAD_RGB);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    SOIL_free_image_data(image);
+    glBindTexture(GL_TEXTURE_2D, 0); // Unbind texture when done, so we won't accidentily mess up our texture.
     
     // ===================
     // Texture 2
@@ -127,9 +129,10 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // Load, create texture and generate mipmaps
 
-    width = 300;
-    height = 900;
-    unsigned char* image = SOIL_load_image("mug.jpeg", &width, &height, 0, SOIL_LOAD_RGB);
+    scope = 1;
+    (width) = 100;
+    height = 100;
+    image = SOIL_load_image("mug.jpeg", &width, &height, &scope, SOIL_LOAD_RGB);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
     SOIL_free_image_data(image);
