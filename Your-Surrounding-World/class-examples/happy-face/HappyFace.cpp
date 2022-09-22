961dc37ca49b52b4f6124fed0893d523ad832d91
drewesch <wheels.esch@gmail.com>
2022-09-22 12:12:33 -0700

    Test

--- Your-Surrounding-World/class-examples/happy-face/HappyFace.cpp
+++ Your-Surrounding-World/class-examples/happy-face/HappyFace.cpp
@@ -127,7 +127,7 @@ int main()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     // Load, create texture and generate mipmaps
 
-    image = SOIL_load_image("white.jpg", &width, &height, 0, SOIL_LOAD_RGB);
+    image = SOIL_load_image("mug.jpeg", &width, &height, 0, SOIL_LOAD_RGB);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
     glGenerateMipmap(GL_TEXTURE_2D);
     SOIL_free_image_data(image);
