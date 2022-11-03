1039b37c3411b69895e050d6a3907834cb209ff6
drewesch <wheels.esch@gmail.com>
2022-11-03 12:37:29 -0700

    Initial keyboard movement with cubes

--- Putting-It-All-Together/Cube/ColorCubeFlyby.cpp
+++ Putting-It-All-Together/Cube/ColorCubeFlyby.cpp
@@ -9,6 +9,25 @@
 #include <GL/glut.h>
 #endif
 #include <cmath>
+#include <iostream>
+
+// A camera.  It moves horizontally in a circle centered at the origin of
+// radius 10.  It moves vertically straight up and down.
+class Camera {
+  double theta;      // determines the x and z positions
+  double y;          // the current y position
+  double dTheta;     // increment in theta for swinging the camera around
+  double dy;         // increment in y for moving the camera up/down
+public:
+  Camera(): theta(0), y(0), dTheta(0.04), dy(0.2) {}
+  double getX() {return 10 * cos(theta) - 10;}
+  double getY() {return y;}
+  double getZ() {return 10 * sin(theta);}
+  void moveRight() {theta += dTheta;}
+  void moveLeft() {theta -= dTheta;}
+  void moveUp() {y += dy;}
+  void moveDown() {if (y > dy) y -= dy;}
+};
 
 // The cube has opposite corners at (0,0,0) and (1,1,1), which are black and
 // white respectively.  The x-axis is the red gradient, the y-axis is the
@@ -43,14 +62,50 @@ void draw() {
 }
 }
 
+// class Cube {
+//   const int NUM_VERTICES;
+//   const int NUM_FACES;
+// public:
+//   double centerx() {return 1 / 2;}
+//   double centerz() {return 1 / 2;}
+//   void create() {
+//     GLint vertices[NUM_VERTICES][3] = {
+//       {0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1},
+//       {1, 0, 0}, {1, 0, 1}, {1, 1, 0}, {1, 1, 1}};
+
+//     GLint faces[NUM_FACES][4] = {
+//       {1, 5, 7, 3}, {5, 4, 6, 7}, {4, 0, 2, 6},
+//       {3, 7, 6, 2}, {0, 1, 3, 2}, {0, 4, 5, 1}};
+
+//     GLfloat vertexColors[NUM_VERTICES][3] = {
+//       {0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 1.0, 0.0}, {0.0, 1.0, 1.0},
+//       {1.0, 0.0, 0.0}, {1.0, 0.0, 1.0}, {1.0, 1.0, 0.0}, {1.0, 1.0, 1.0}};
+    
+//     glBegin(GL_QUADS);
+//     for (int i = 0; i < NUM_FACES; i++) {
+//       for (int j = 0; j < 4; j++) {
+//         glColor3fv((GLfloat*)&vertexColors[faces[i][j]]);
+//         glVertex3iv((GLint*)&vertices[faces[i][j]]);
+//       }
+//     }
+//     glEnd();
+//   }
+// };
+
 // Display and Animation. To draw we just clear the window and draw the cube.
 // Because our main window is double buffered we have to swap the buffers to
 // make the drawing visible. Animation is achieved by successively moving our
 // camera and drawing. The function nextAnimationFrame() moves the camera to
 // the next point and draws. The way that we get animation in OpenGL is to
 // register nextFrame as the idle function; this is done in main().
+
+
+Camera camera;
+bool stopped = false;
+
 void display() {
   glClear(GL_COLOR_BUFFER_BIT);
+  // gluLookAt(camera.getX(), camera.getY(), camera.getZ(), 1.0, 0.0, 1.0, 0.0, 1.0, 0.0);
   Cube::draw();
   glFlush();
   glutSwapBuffers();
@@ -66,9 +121,17 @@ void timer(int v) {
   glLoadIdentity();
   
   //IMPORTANT --> moving the camera. Will need to change to stop and rotate.
-  gluLookAt(8*cos(u), 7*cos(u)-1, 4*cos(u/3)+2, .5, .5, .5, cos(u), 1, 0);
+  gluLookAt(8*cos(u), 7*cos(u)-1, 4*cos(u/3) + 2,
+        .5 + camera.getX(), .5 + camera.getY(), .5 + camera.getZ(),
+        cos(u), 1, 0);
+  if (stopped != 0) {
+
+  } else {
+
+  }
   //gluLookAt(8, 7, 4, .5, .5, .5, cos(u), 1, 0); //Ex. Static camera position looking at positon .5,.5,.5
-  
+  // printf("Camera current pos: x - %f y - %f z - %f", camera.getX(), camera.getY(), camera.getZ());
+
   glutPostRedisplay();
   glutTimerFunc(1000/60.0, timer, v);
 }
@@ -83,6 +146,21 @@ void reshape(int w, int h) {
   glLoadIdentity();
   gluPerspective(60.0, GLfloat(w) / GLfloat(h), 0.5, 40.0);
   glMatrixMode(GL_MODELVIEW);
+}
+
+void special(unsigned char key, int, int) {
+  // Keyboard functionality
+  switch (key) {
+    case 'u': if(stopped){camera.moveUp();} break;
+    case 'd': if(stopped){camera.moveDown();} break;
+    case 's': stopped = true; break;
+    case 'c': stopped = false; break;
+    // case GLUT_KEY_LEFT: camera.moveLeft(); break;
+    // case GLUT_KEY_RIGHT: camera.moveRight(); break;
+    // case GLUT_KEY_UP: camera.moveUp(); break;
+    // case GLUT_KEY_DOWN: camera.moveDown(); break;
+  }
+  glutPostRedisplay();
 }
 
 // Application specific initialization:  The only thing we really need to do
@@ -102,6 +180,7 @@ int main(int argc, char** argv) {
   glutReshapeFunc(reshape);
   glutTimerFunc(100, timer, 0);
   glutDisplayFunc(display);
+  glutKeyboardFunc(special);
   init();
   glutMainLoop();
 }
