21334058b64419dfd212e0357a32d3bbb1e7ce12
drewesch <wheels.esch@gmail.com>
2022-11-03 22:59:32 -0700

    Keyboard movement for triangles

--- Putting-It-All-Together/Triangles/CheckeredTriangles.cpp
+++ Putting-It-All-Together/Triangles/CheckeredTriangles.cpp
@@ -9,6 +9,8 @@
 #include <GL/glut.h>
 #endif
 #include <cstdlib>
+#include <cmath>
+#include <iostream>
 
 // Define a 2 x 2 red and yellow checkered pattern using RGB colors.
 #define red {0xff, 0x00, 0x00}
@@ -19,9 +21,27 @@ GLubyte texture[][3] = {
     yellow, red,
 };
 
+class Camera {
+  double x;      // determines the x and z positions
+  double y;          // the current y position
+  double dx;     // increment in theta for swinging the camera around
+  double dy;         // increment in y for moving the camera up/down
+public:
+  Camera(): x(0), y(0), dx(0.3), dy(0.3) {}
+  double getX() {return x;}
+  double getY() {return y;}
+  void moveRight() {glTranslatef(-dx, 0, 0);}
+  void moveLeft() {glTranslatef(dx, 0, 0);}
+  void moveUp() {glTranslatef(0, -dy, 0);}
+  void moveDown() {glTranslatef(0, dy, 0);}
+};
+
+Camera camera;
 GLfloat rotate = 0.0f;
 GLfloat pause = 1.0f;
 GLfloat zoom = 1.0f;
+GLfloat moveX = 0.0f;
+GLfloat moveY = 0.0f;
 //GLfloat height = -1.0f;
 bool keys[1024];
 
@@ -31,9 +51,9 @@ void reshape(int width, int height) {
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   gluPerspective(80, GLfloat(width)/height, 1, 40);
+  gluLookAt(2, -1, 5, 0, 0, 0, 0, 1, 0);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  gluLookAt(2, -1, 5, 0, 0, 0, 0, 1, 0);
   glEnable(GL_TEXTURE_2D);
   glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
   glTexImage2D(GL_TEXTURE_2D,
@@ -55,6 +75,7 @@ if(rotate>360.f)
 {
 rotate-=360;
 }
+
 glutPostRedisplay();
 glutTimerFunc(25,update,0);
 }
@@ -65,7 +86,7 @@ glutTimerFunc(25,update,0);
 void display() {
   glClear(GL_COLOR_BUFFER_BIT);
   glPushMatrix();
-  glRotatef( rotate, 0.0, 0.0, 1.0 );
+  glRotatef(rotate, 0.0, 0.0, 1.0);
   glScalef(zoom,zoom,zoom);
   
   glBegin(GL_TRIANGLES);
@@ -94,8 +115,10 @@ void special(unsigned char key, int, int) {
     case 'c': pause = 1.0f; break;
     case '=': zoom += 0.2f; break;
     case '-': zoom -= 0.2f; break;
-    //case 'u': height += 0.2f; break;
-    //case 'd': height -= 0.2f; break;
+    case 'u': camera.moveUp(); break;
+    case 'd': camera.moveDown(); break;
+    case 'l': camera.moveLeft(); break;
+    case 'r': camera.moveRight(); break;
   }
   glutPostRedisplay();
 }
@@ -106,7 +129,6 @@ int main(int argc, char** argv) {
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
   glutInitWindowSize(520, 390);
   glutCreateWindow("Textured Triangles");
-  //glutTimerFunc(100, timer, 0);
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
   glutTimerFunc(25,update,0);
